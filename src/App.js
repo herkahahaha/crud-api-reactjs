84148f78f423ec7752e66fdd2a9aa96abac6ecb6
Herkahahaha <herkahahaha@gmail.com>
2019-11-15 03:10:54 +0700

    add Delete method and realtime database

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ class App extends React.Component {
   state = {
     dataApi: []
   };
+
+  getData = () => {
+    // GET data
+    axios.get("http://localhost:3003/posts").then(res =>
+      this.setState({
+        dataApi: res.data.slice(0, 10)
+      })
+    );
+  };
   componentDidMount = () => {
     // fetch("https://jsonplaceholder.typicode.com/posts")
     //   .then(response => response.json())
@@ -13,14 +22,16 @@ class App extends React.Component {
     //       dataApi: res
     //     })
     //   );
-
-    // GET data
-    axios.get("https://jsonplaceholder.typicode.com/posts").then(res =>
-      this.setState({
-        dataApi: res.data.slice(0, 10)
-      })
-    );
+    this.getData();
   };
+
+  handleRemove = e => {
+    console.log(e.target.value);
+    fetch(`http://localhost:3003/posts/${e.target.value}`, {
+      method: "DELETE"
+    }).then(res => this.getData());
+  };
+
   render() {
     return (
       <div className="App">
@@ -30,6 +41,9 @@ class App extends React.Component {
             <div key={index}>
               <h4> {data.title} </h4>
               <p>{data.body}</p>
+              <button value={data.id} onClick={this.handleRemove}>
+                Delete
+              </button>
             </div>
           );
         })}
