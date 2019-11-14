fa19f399605a63128692d1a0bee3dfb3bbf64c96
Herkahahaha <herkahahaha@gmail.com>
2019-11-15 02:17:00 +0700

    Get Data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import React from "react";
+import axios from "axios";
 
+class App extends React.Component {
+  state = {
+    dataApi: []
+  };
+  componentDidMount = () => {
+    // fetch("https://jsonplaceholder.typicode.com/posts")
+    //   .then(response => response.json())
+    //   .then(res =>
+    //     this.setState({
+    //       dataApi: res
+    //     })
+    //   );
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hellowww</h1>
-    </div>
-  );
+    // GET data
+    axios.get("https://jsonplaceholder.typicode.com/posts").then(res =>
+      this.setState({
+        dataApi: res.data.slice(0, 10)
+      })
+    );
+  };
+  render() {
+    return (
+      <div className="App">
+        <h1>Belajar API</h1>
+        {this.state.dataApi.map((data, index) => {
+          return (
+            <div key={index}>
+              <h4> {data.title} </h4>
+              <p>{data.body}</p>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
